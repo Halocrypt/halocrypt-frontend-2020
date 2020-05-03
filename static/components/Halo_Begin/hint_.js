c7d0622fe9e86b61ee5f6cb50d6e39ad241a0cfc
me <bhavesh.pareek36@gmail.com>
2020-05-03 20:01:14 +0530

    s p a c i n g

--- static/components/Halo_Begin/hint_.js
+++ static/components/Halo_Begin/hint_.js
@@ -1,6 +1,6 @@
 export default function Halo_Begin() {
   return (
-    <div style={{ padding: "8px", textAlign: "left" }}>
+    <div style={{ padding: "8px", textAlign: "left", marginLeft: "3rem" }}>
       <div class="bold clr heading-text" style={{ fontSize: "3rem" }}>
         Introduction
       </div>
